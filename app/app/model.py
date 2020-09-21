fd44e17d59ae22d832ff4d464c63db383f764964
yehonathanraouf <yehonathan.raouf@bluevine.com>
2020-09-21 23:36:38 +0300

    add constraints to model.py

--- app/app/model.py
+++ app/app/model.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Authors(models.Model):
@@ -6,5 +7,14 @@ class Authors(models.Model):
     second_name = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(first_name__isnull=False, second_name__isnull=False) | Q(company_name__isnull=False),
+                name='authors_check_not_empty'),
+            models.UniqueConstraint(fields=['first_name', 'second_name'], name='authors_ind_1',
+                                    condition=Q(company_name__isnull=True)),
+            models.UniqueConstraint(fields=['company_name'], name='authors_ind_2',
+                                    condition=Q(company_name__isnull=False))
 
-
+        ]
